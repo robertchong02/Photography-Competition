16ef7077859a32463643a3e1599ace09d91b40cd
Yeongweiii <lyeongwei@gmail.com>
2022-05-20 15:28:49 +0800

    link updated

--- php/compadminPost.php
+++ php/compadminPost.php
@@ -5,6 +5,8 @@
   <!--Link to CSS-->
   <link rel = "stylesheet" href = "../css/adminaddcomp.css">
 
+  <!--Link to picture-->
+  <link rel = "icon" type = image/png href ="../image/logo.png">
   <title>Add competition</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
@@ -19,7 +21,7 @@
     <a href="#Logout">Logout</a>
     <a href="#Winner">Winner</a>
     <a class="active" href="#home">Home</a>
-    <img class="logo" style="float: left;" src="logo.png">
+    <img class="logo" style="float: left;" src="../image/logo.png">
     <p style="float: left;">APU Photography Competition</p>
 
   </div>
