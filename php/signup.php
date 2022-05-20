682f480a2901edb96ab297744a5ecdce669afb2c
Yeongweiii <lyeongwei@gmail.com>
2022-05-20 17:56:54 +0800

    signup updated

---
+++ php/signup.php
@@ -0,0 +1,78 @@
+﻿<?php
+include("connect.php");
+
+//Customer Info and Validations
+$Fname = mysqli_real_escape_string($con, $_POST['firstname']);
+$Lname = mysqli_real_escape_string($con, $_POST['lastname']);
+$Username = mysqli_real_escape_string($con, $_POST['username']);
+$Email = mysqli_real_escape_string($con, $_POST['email']);
+$ContactNumber = mysqli_real_escape_string($con, $_POST['phone']);
+$Gender = mysqli_real_escape_string($con, $_POST['gender']);
+$DoB = mysqli_real_escape_string($con, $_POST['dob']);
+$Nationality = mysqli_real_escape_string($con, $_POST['nationality']);
+$Password = mysqli_real_escape_string($con, $_POST['password']);
+$ConfirmPassword = mysqli_real_escape_string($con, $_POST['confirmpassword']);
+
+
+// MySQL Queries
+$insert = "INSERT INTO usersignup (Fname, Lname, Username, Email, ContactNumber, Gender, DoB, Nationality, Password, ConfirmPassword) VALUES ('$_POST[firstname]', '$_POST[lastname]', '$_POST[username]', , '$_POST[email]', '$_POST[phone]'), '$_POST[gender]', '$_POST[dob]', '$_POST[nationality]'), '$_POST[password]', '$_POST[confirmpassword]';";
+$check_username = "SELECT * FROM usersignup WHERE Username = '$Username'";
+$check_email = "SELECT * FROM usersignup WHERE Email = '$Email'";
+
+$validate_username = mysqli_query($con, $check_username);
+$validate_email = mysqli_query($con, $check_email);
+
+// PHP
+if($Password == $ConfirmPassword)
+{
+    if(mysqli_num_rows($validate_email) == 0) 
+    {
+        if(mysqli_num_rows($validate_username) == 0)
+        {
+            if(mysqli_query($con, $insert))
+            {
+                echo 
+                '<script>
+                alert("Thank you for signing up with APU Photography Club. Please proceed to login.");
+                
+                </script>';
+            }
+            else
+            {
+                echo 
+                '<script>
+                alert("Sign up failed. Please try again. Sorry for the inconvience.");
+                
+                </script>';
+            }
+        }
+        else
+        {
+            echo 
+            '<script>
+            alert("Username is already taken. Please try a different username.");
+            window.location.href = "signup.php";
+            </script>';
+        }
+    } 
+    else
+    {
+        echo 
+        '<script>
+        alert("There is an existing email address similiar to what you typed. Please login or try a different email address.");
+        window.location.href = "signup.php";
+        </script>';
+    }
+} else 
+{
+    echo 
+    '<script>
+    alert("Passwords are not matching.");
+    window.location.href = "signup.php";
+    </script>';
+}
+
+// Close connection to database
+mysqli_close($con);
+
+?>
---
+++ php/usersignup.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <!--Link to CSS-->
+  <link rel = "stylesheet" href = "../css/signup.css">
+<title>Sign up</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body style="background-image:url('bg1.png')">
+<div class="topnav">
+    <a href="#Sign Up">Sign Up</a>
+    <a href="login.html">Login</a>
+    <a href="#Winner">Winner</a>
+    <a href="#About">About Us</a>
+    <a class="active" href="main.html">Home</a>
+    <img class="logo" style="float: left;" src = "logo.png">
+    <p style="float: left;">APU Photography Competition</p>
+</div>
+
+<div class="title" style="background-color:#A7CAD7"><h2 id="Sign Up" >Sign Up</h2><br/>
+    <form method="POST" action="signup.php">
+    <table>
+    <tr>
+		<td style="text-align:left">First Name</td>
+ 		<td>:</td>
+ 		<td><input type="text" name="firstname" size="20" maxlength="15"/></td>
+ 	</tr>
+ 	<tr>
+ 		<td style="text-align:left">Last Name</td>
+ 		<td>:</td>
+ 		<td><input type="text" name="lastname" size="20" maxlength="15"/></td>
+ 	</tr>
+ 	<tr>
+ 		<td style="text-align:left">Username</td>
+ 		<td>:</td>
+ 		<td><input type="text" name="username" size="20" maxlength="15"/></td>
+ 	</tr>
+ 	<tr>
+ 		<td style="text-align:left">Email</td>
+ 		<td>:</td>
+ 		<td><input type="email" id="email" size="20" name="email"/></td>
+	</tr>
+ 	<tr>
+ 		<td style="text-align:left">Contact Number</td>
+ 		<td>:</td>
+ 		<td><input type="tel" id="phone" name="phone" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"/></td>
+ 	</tr>
+ 	<tr>
+ 		<td style="text-align:left">Gender</td>
+ 		<td>:</td>
+ 		<td>	<input type="radio" name="gender" value="M"/>Male
+ 				<input type="radio" name="gender" value="F"/>Female
+ 		</td>
+	</tr>
+ 	<tr>
+ 		<td style="text-align:left">Date of Birth</td>
+ 		<td>:</td>
+ 		<td><input type="date" id="dob" name="dob"/></td>
+ 	</tr>
+ 	<tr>
+ 		<td style="text-align:left">Nationality</td>
+ 		<td>:</td>
+ 		<td><input type="text" name="nationality" size="20" maxlength="15"/></td>
+ 	</tr>
+ 	<tr>
+ 		<td style="text-align:left">Password</td>
+ 		<td>:</td>
+ 		<td><input type="text" name="password" size="20" maxlength="15"/></td>
+ 	</tr>
+ 	<tr>
+ 		<td style="text-align:left">Confirm Password</td>
+ 		<td>:</td>
+ 		<td><input type="text" name="confirmpassword" size="20" maxlength="15"/></td>
+ 	</tr>
+ 		<td></td>
+ 	<tr></tr>
+ 	</table>
+    <input type="submit" value="Sign Up">
+    </form><br/><br/>
+
+<!-- <button type="button" class="button button1 btn btn-primary">Sign Up</button>
+            <div class = "popup" id="popup">
+                <h2>Signed Up Successfully</h2>
+                <p>Please click on "Login as User" to login to your account.</p>
+                <button type="button" onclick="closePopup()">OK</button>
+            </div><br/><br/>
+            
+<a href="login.html">  
+    <button type="button" class="btn btn-primary">Login as User</button>  
+</a> -->
+
+
+</div>
+
+
+</body>
+</html>
