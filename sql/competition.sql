5895e25dc9f3f4a75adff6bd5f0f330fc743a6a6
Yeongweiii <lyeongwei@gmail.com>
2022-05-21 10:55:43 +0800

    competition sql modified

--- sql/competition.sql
+++ sql/competition.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 20, 2022 at 09:41 AM
+-- Generation Time: May 21, 2022 at 04:47 AM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 7.4.29
 
