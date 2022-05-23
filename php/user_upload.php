b9c4a7f05b9b8a889a7007b55e59378515f92d29
Robert <chongdicsum@gmail.com>
2022-05-23 17:20:37 +0800

    working on user_upload

--- php/user_upload.php
+++ php/user_upload.php
@@ -1,28 +1,44 @@
 <?php
 include("connect.php");
+include("session.php");
+    $customerID = $_SESSION['ID'];
+
+	$CompetitionID = mysqli_query($con, "SELECT * FROM competition WHERE CompetitionID = '$CompetitionID';");
+
+// include("session.php");
+//     $customerID = $_SESSION['ID'];
 
 // if ($con->connect_error) {
 //     die("Connection failed: " . $con->connect_error);
 //  }
 //    echo "Connected successfully";
 
-$uploadImage = $_FILES['filename']['tmp_name'];
+$uploadImage = $_FILES['image']['tmp_name'];
 
-$file = file_get_contents($uploadImage);
+$img = file_get_contents($uploadImage);
 
-$sql = "INSERT INTO 'content' (ContentImage ,ContentTitle ,ContentDescription ,ParticipantName)
-VALUES(?,'$_POST[conTitle]','$_POST[conDesc]', '$_POST[userName]')" ;
+$sql = "INSERT INTO content (CompetitionID, ContentImage ,ContentTitle ,ContentDescription ,ParticipantName)
+VALUES
+(9,?,'$_POST[conTitle]','$_POST[conDesc]', '$_POST[userName]')" ;
 
 
 $stmt = mysqli_prepare($con,$sql);
 
-mysqli_stmt_bind_param($stmt,"s",$file);
+mysqli_stmt_bind_param($stmt,"s",$img);
 
 mysqli_stmt_execute($stmt);
 
 $check = mysqli_stmt_affected_rows($stmt);
 
 echo $check;
+if($check == 1) {
+    echo '<script> alert ("New coffee beans in stock! Image uploaded.");
+    </script>';
+
+} else {
+    echo '<script> alert ("Upload failed.");
+    </script>';
+}
 
 
 mysqli_close($con);
