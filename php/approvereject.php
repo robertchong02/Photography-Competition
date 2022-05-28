79e6ceffcfb1afac66b771f89dbadb255fa71865
PhillipWong0627 <phillipwong0627@gmail.com>
2022-05-28 10:50:22 +0800

    ds

--- php/approvereject.php
+++ php/approvereject.php
@@ -37,7 +37,7 @@ $deleteQuery = mysqli_query($con, "DELETE FROM content WHERE contentID = '$selec
 if ($deleteQuery) {
     echo '<script>
     alert ("Successfully reject content!");
-    window.location.href= "verifycontent.php";
+    window.location.href= "adminverifycontent.php";
     </script>';
     
 } else {
