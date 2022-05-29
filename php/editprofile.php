6a4872d77f5dc94b0836ba5520af2b7942a23e28
ivyyy02 <ivytamshijie@gmail.com>
2022-05-29 14:41:36 +0800

    updated

--- php/editprofile.php
+++ php/editprofile.php
@@ -13,21 +13,21 @@ $customerID = $_SESSION['Customer_ID'];
 <title>Profile</title>
 </head>
 
-<body style="background-image:url('../image/bg1.png')">
+<body style="background-image:url('../image/bg.gif');font-family:Old Standard TT, serif">
 <div class="topnav">
   <a href="logout.php">Logout</a>
-  <a href="#Profile">Profile</a>
-  <a href="userhome.php">Winner</a>
-  <a href="userhome.php">About Us</a>
-    <a class="active" href="userhome.php">Home</a>
-    <img class="logo" style="float: left;" src = "../image/logo.png">
-    <p style="float: left;">AP Photography Club</p>
+  <a href="editprofile.php">Profile</a>
+  <a href="#Winner">Winner</a>
+  <a href="#About">About Us</a>
+  <a class="active" href="main.php">Home</a>
+  <a style="float:left; position:relative; bottom:20px;"><img class="logo" src = "../image/logo.png"></a>
+  <a style="font-family: Comic San; font-weight:bold; position:relative; right:10cm" >AP Photography Club</a>
 </div>
 <h2 style="text-align:center;">USER PROFILE</h2>	
 <nav>
 <!-- <div id="profile">
 </div> -->
-<div id="profiledata" style="background-color:#A7CAD7; margin-left:300px; margin-bottom:30px">
+<div id="profiledata" style="background-color:#A7CAD7; margin:auto">
 <form action="update.php?ID=<?php echo $customerID?>" method = "POST">
 <?php
     $getCustomerInfo = mysqli_query($con, "SELECT * FROM user WHERE Customer_ID = '$customerID';");
@@ -69,10 +69,6 @@ $customerID = $_SESSION['Customer_ID'];
     }
 
 ?>
-
-
-
-
 </form>
 </div>
 </nav>
@@ -80,9 +76,29 @@ $customerID = $_SESSION['Customer_ID'];
 <p align="left">
 	<h2 style="text-align:left">Select photo:</h2>
     <input type="file" id="myfile" name="myfile" style="background-color:transparent">
-</p> -->
+</p> 
+</nav>-->
+<!-- footer -->
+<div class="footer" style="margin-top:100px" >
+	<div class="flex-container" style="align-items:center; justify-content:center; text-align:left">
+	<div style="padding-top:20px"> 
+		<img class="logo" style="width:120px; height:100px" src = "../image/logo.png"></br></br>
+	</div>
+	<div style="font-size:14px; padding-bottom:40px">
+		<p><b style="font-size:14px">Asia Pacific University</b></p>
+		Jalan Teknologi 5, </br>
+		Taman Teknologi Malaysia,</br>
+		57000 Kuala Lumpur,</br>
+		Wilayah Persekutuan Kuala Lumpur.
+	</div>
+	</div></br>
+	<div>
+		<p style="text-align:center">Contact us by clicking
+		<a href="https://api.whatsapp.com/send?phone=60163543712&text=Hello,%20I%20have%20questions%20regarding%20the%20AP%20Photo%20Competition%20%3E.%3C">here</a>
+		</p>
+	</div>
+	<div style="text-align:center; font-size:10px">Copyright &copy; 2022 AP Photography Club</div></br>
+</div></br>
 
-
-</nav>
 </body>
 </html>
